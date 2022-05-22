7d579e6608085f33767785f2bcee5aadc9fe516d
Timofey <54998271+Berdikin@users.noreply.github.com>
2022-05-22 17:36:45 +0300

    клиентская и серверная часть

---
+++ src/Program.cs
@@ -0,0 +1,38 @@
+﻿using LiteNetLib;
+using LiteNetLib.Utils;
+
+
+public static class Programm
+{
+    public static void Main(string[] args)
+    {
+        EventBasedNetListener listener = new EventBasedNetListener();
+        NetManager server = new NetManager(listener);
+        server.Start(9050 /* port */);
+
+        listener.ConnectionRequestEvent += request =>
+        {
+            if(server.ConnectedPeersCount < 10 /* max connections */)
+                request.AcceptIfKey("SomeConnectionKey");
+            else
+                request.Reject();
+        };
+
+        listener.PeerConnectedEvent += peer =>
+        {
+            Console.WriteLine("We got connection: {0}", peer.EndPoint); // Show peer ip
+            NetDataWriter writer = new NetDataWriter();                 // Create writer class
+            writer.Put("Hello client!");                                // Put some string
+            peer.Send(writer, DeliveryMethod.ReliableOrdered);             // Send with reliability
+        };
+        Console.WriteLine("Server Started!");
+        var stop = false;
+        Console.CancelKeyPress += (a, b) => stop = true;
+        while (!stop)
+        {
+            server.PollEvents();
+            Thread.Sleep(15);
+        }
+        server.Stop();
+    }
+}

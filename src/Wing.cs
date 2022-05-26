9466d607007ce18ec21e0818af040dc37180983e
Timofey <54998271+Berdikin@users.noreply.github.com>
2022-05-26 16:01:07 +0300

    Update and rename Wing (4).cs to Wing.cs

--- src/Wing.cs
+++ src/Wing.cs
@@ -4,17 +4,13 @@ using System;
 using System.Text.RegularExpressions;
 
 public class Wing : MonoBehaviour
-{
-    public GameObject wing;
+{ 
 
     void Start()
     {
-        gameObject.tag = "Wing";
     }
 
     void Update()
-    {
-       
+    {  
     }
 }
-

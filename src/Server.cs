b6d8e919260bc4509178387312e4ec3bd10d1140
Timofey <54998271+Berdikin@users.noreply.github.com>
2022-05-26 18:12:50 +0300

    Update Server.cs

--- src/Server.cs
+++ src/Server.cs
@@ -14,6 +14,7 @@ public static class Programm
     public static void Main(string[] args)
     {
         NetPacketProcessor netProcessor = new NetPacketProcessor();
+        NetPeer first = null;
 
         EventBasedNetListener netListener = new EventBasedNetListener();
         NetManager netManager = new NetManager(netListener);
@@ -25,20 +26,28 @@ public static class Programm
         };
 
         netListener.NetworkReceiveEvent += (client, reader, deliveryMethod) => {
+            first = client;
             netProcessor.ReadAllPackets(reader, client);
         };
 
         netListener.PeerConnectedEvent += client =>
         {
-            Console.WriteLine("We got connection: {0}", client.EndPoint); // Show peer ip
+            Console.WriteLine("We got connection: {0}", client.EndPoint);
         };
 
         netProcessor.SubscribeReusable<PacketAboutObject>((packet) =>
         {
             Console.WriteLine("Got a packet from client!");
-            Console.WriteLine("Object {0} now have a position: {1}, {2}, {3}", packet.NameOfObject, packet.XCoordinate, packet.YCoordinate, packet.ZCoordinate);
-            netManager.SendToAll(netProcessor.Write(new PacketAboutObject() 
-            { XCoordinate = packet.XCoordinate , YCoordinate = packet.YCoordinate , ZCoordinate = packet.ZCoordinate , NameOfObject = packet.NameOfObject }), DeliveryMethod.ReliableOrdered);
+            
+            //Console.WriteLine("Object {0} now have a position: {1}, {2}, {3}", packet.NameOfObject, packet.XCoordinate, packet.YCoordinate, packet.ZCoordinate);
+            foreach (NetPeer c in netManager.ConnectedPeerList) 
+            {
+                if(c != first)
+                {
+                    netProcessor.Send(c, new PacketAboutObject()
+                    {XCoordinate = packet.XCoordinate, YCoordinate = packet.YCoordinate, ZCoordinate = packet.ZCoordinate, NameOfObject = packet.NameOfObject }, DeliveryMethod.ReliableOrdered);
+                }
+            } 
         });
 
         Console.WriteLine("Server Started!");
@@ -47,7 +56,7 @@ public static class Programm
         while (!stop)
         {
             netManager.PollEvents();
-            Thread.Sleep(500);
+            Thread.Sleep(10);
         }
         netManager.Stop();
     }
